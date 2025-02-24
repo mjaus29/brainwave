eff80776ac1d7a4776dcd5aaf6dd083d3975d830
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-02-24 10:26:50 +0800

    add new design components and update Header with navigation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+// import Button from "./components/Button";
 import Header from "./components/Header";
 
 const App = () => {
